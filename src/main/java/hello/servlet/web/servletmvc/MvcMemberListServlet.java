ad5bfc4d4b30b52fe200ac186fd490c584907b84
gogoack <gogoack@gmail.com>
2022-11-25 18:20:39 +0900

    11.25-2

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -15,17 +15,11 @@ import java.util.List;
 @WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
 public class MvcMemberListServlet extends HttpServlet {
 
-    private MemberRepository memberRepository = MemberRepository.getInstance();
+
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        List<Member> members = memberRepository.findAll();
 
-        request.setAttribute("members", members);
-
-        String viewPath = "/WEB-INF/views/members.jsp";
-        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request, response);
     }
 }
