2c5138d3d7adbefdc5151c83de21b4c834881800
gogoack <gogoack@gmail.com>
2022-12-13 15:16:12 +0900

    12.13

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -2,11 +2,14 @@ package hello.servlet.web.frontcontroller.v5;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,25 +33,33 @@ public class FrontControllerServletV5 extends HttpServlet {
         initHandlerAdapters();
     }
     private void initHandlerMappingMap() {
-        handlerAdapters.add(new ControllerV3HandlerAdapter());
+//        handlerAdapters.add(new ControllerV3HandlerAdapter());
 
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        // V4 추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // MemberFormControllerV3
         Object handler = getHandler(request);
         if(handler == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
+        //ControllerV4HandlerAdapter
         MyHandlerAdapter adapter = getHandlerAdapter(handler);
 
         ModelView mv = adapter.handle(request, response, handler);
@@ -67,12 +78,13 @@ public class FrontControllerServletV5 extends HttpServlet {
     }
 
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        // MemberFormControllerV4
         for (MyHandlerAdapter adapter : handlerAdapters) {
             if(adapter.supports(handler)) {
                 return adapter;
             }
         }
-        throw new IllegalArgumentException("hander adapter를 찾을수 없습니다" + handler);
+        throw new IllegalArgumentException("handler adapter를 찾을수 없습니다" + handler);
     }
 
     private MyView viewResolver(String viewName) {
--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,8 +1,9 @@
 package hello.servlet.web.frontcontroller.v5.adapter;
 
 import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import org.springframework.ui.Model;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,25 +12,26 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
-
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object handler) {
-        // MemberFormControllerV3
-        return (handler instanceof ControllerV3);
+        return (handler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-        // MemberFormControllerV3
-        ControllerV3 controller = (ControllerV3) handler;
+        ControllerV4 controller = (ControllerV4) handler;
 
         Map<String, String> paraMap = createParaMap(request);
-        ModelView mv = controller.process(paraMap);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paraMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
 
         return mv;
     }
-
     private Map<String, String> createParaMap(HttpServletRequest request) {
         //paraMap
         Map<String, String> paraMap = new HashMap<>();
@@ -37,7 +39,4 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
                 .forEachRemaining(paramName -> paraMap.put(paramName, request.getParameter(paramName)));
         return paraMap;
     }
-
-
 }
-
